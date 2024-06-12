d8815f9ae3d54d2931abd7a7a9f928cd034a03a1
Valentin Pozniak <waliker448@gmail.com>
2024-06-12 21:05:21 +0300

    add class Task in models.py

--- manager/models.py
+++ manager/models.py
@@ -27,3 +27,28 @@ class Worker(AbstractUser):
         Position,
         on_delete=models.CASCADE
     )
+
+
+class Task(models.Model):
+    PRIORITY = (
+        ("Low Priority", "Immediate priority and quick completion"),
+        ("Standard", "Normal completion pace"),
+        ("Fast Track", "Lower urgency")
+    )
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        choices=PRIORITY,
+        default="Standard"
+    )
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE
+    )
+    assignees = models.ManyToManyField(
+        Worker,
+        related_name="assignees"
+    )
