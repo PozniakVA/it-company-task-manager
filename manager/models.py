145f57ffa67102a1aaeceddb39dc4b2766fd96c4
Valentin Pozniak <waliker448@gmail.com>
2024-06-12 20:17:09 +0300

    add class Position in models.py with method str & name with max_lenght=100 & class Meta with ordering by name

--- manager/models.py
+++ manager/models.py
@@ -9,3 +9,13 @@ class TaskType(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
