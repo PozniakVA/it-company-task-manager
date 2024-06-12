f7e4db78970c8102091dd3e564aa897702fcfae1
Valentin Pozniak <waliker448@gmail.com>
2024-06-12 22:04:48 +0300

    add AUTH_USER_MODEL & change TIME_ZONE from UTC to Europe/Kiev

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -100,13 +100,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "manager.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
