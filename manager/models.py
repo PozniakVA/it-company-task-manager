aaf471b5da58edf8fb3d778975e78984aac48757
Valentin Pozniak <waliker448@gmail.com>
2024-06-12 21:22:55 +0300

    add method str in class Worker in models.py

--- manager/models.py
+++ manager/models.py
@@ -28,6 +28,9 @@ class Worker(AbstractUser):
         on_delete=models.CASCADE
     )
 
+    def __str__(self) -> str:
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
 
 class Task(models.Model):
     PRIORITY = (
