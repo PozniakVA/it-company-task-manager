9d5e2793f55cba7733cfa02e09d4eb25420d043d
Valentin Pozniak <waliker448@gmail.com>
2024-06-13 14:08:41 +0300

    create index and index.html

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from manager.models import Worker, Task
+
+
+def index(request) -> HttpResponse:
+    num_workers = Worker.objects.count()
+    num_not_completed_tasks = Task.objects.filter(is_completed=False).count()
+    num_not_completed_fast_track_tasks = Task.objects.filter(
+        is_completed=False,
+        priority="Fast Track"
+    ).count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_workers": num_workers,
+        "num_not_completed_tasks": num_not_completed_tasks,
+        "num_not_completed_fast_track_tasks": num_not_completed_fast_track_tasks,
+        "num_visits": num_visits,
+    }
+    return render(request, "manager/index.html", context=context)
