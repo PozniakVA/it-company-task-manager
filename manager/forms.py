e36c14134279f8e17a8afc748c4811854108cede
Valentin Pozniak <waliker448@gmail.com>
2024-06-18 13:37:31 +0300

    create search for task list

--- manager/forms.py
+++ manager/forms.py
@@ -48,3 +48,12 @@ class WorkerSearchForm(forms.Form):
         required=False,
         widget=forms.TextInput(attrs={"placeholder": "Search by username"})
     )
+
+
+class TaskSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        label="",
+        required=False,
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"})
+    )
--- manager/views.py
+++ manager/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from manager.forms import TaskForm, TaskAssignDeleteForm, WorkerSearchForm
+from manager.forms import (
+    TaskForm,
+    TaskAssignDeleteForm,
+    WorkerSearchForm,
+    TaskSearchForm
+)
 from manager.models import Worker, Task, Position, TaskType
 
 
@@ -52,6 +57,19 @@ class WorkerDetailView(generic.DetailView):
 class TaskListView(generic.ListView):
     model = Task
     paginate_by = 3
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TaskListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = TaskSearchForm(initial={"name": name})
+        return context
+
+    def get_queryset(self):
+        queryset = Task.objects.all()
+        form = TaskSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(name__icontains=form.cleaned_data["name"])
+        return queryset
 
 
 class TaskDetailView(generic.DetailView):
